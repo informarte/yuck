4daf9d7b84a673aad726182d8469ef86ce44fc64
Michael Marte <informarte@freenet.de>
2018-09-22 12:44:02 +0200

    Put model size into result database

--- scripts/create-results-db.py
+++ scripts/create-results-db.py
@@ -12,14 +12,14 @@ import sqlite3
 from itertools import repeat
 
 def createDb(cursor):
-    cursor.execute('CREATE TABLE IF NOT EXISTS result (run TEXT NOT NULL, suite TEXT NOT NULL, problem TEXT NOT NULL, model TEXT NOT NULL, instance TEXT NOT NULL, problem_type TEXT NOT NULL CONSTRAINT result_problem_type_constraint CHECK (problem_type IN ("MIN", "MAX", "SAT")), optimum INT, high_score INT, solved INT NOT NULL CONSTRAINT result_solved_constraint CHECK (solved in (0, 1)), violation INT CONSTRAINT result_violation_constraint CHECK (violation >= 0), quality INT, runtime_in_seconds DOUBLE CONSTRAINT result_runtime_in_seconds_constraint CHECK (runtime_in_seconds >= 0), moves_per_second DOUBLE CONSTRAINT result_moves_per_second_constraint CHECK (moves_per_second >= 0), CONSTRAINT result_unique_constraint UNIQUE (run, problem, model, instance) ON CONFLICT IGNORE)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS result (run TEXT NOT NULL, suite TEXT NOT NULL, problem TEXT NOT NULL, model TEXT NOT NULL, instance TEXT NOT NULL, problem_type TEXT NOT NULL CONSTRAINT result_problem_type_constraint CHECK (problem_type IN ("MIN", "MAX", "SAT")), optimum INT, high_score INT, solved INT NOT NULL CONSTRAINT result_solved_constraint CHECK (solved in (0, 1)), violation INT CONSTRAINT result_violation_constraint CHECK (violation >= 0), quality INT, runtime_in_seconds DOUBLE CONSTRAINT result_runtime_in_seconds_constraint CHECK (runtime_in_seconds >= 0), moves_per_second DOUBLE CONSTRAINT result_moves_per_second_constraint CHECK (moves_per_second >= 0), number_of_variables INT NOT NULL CONSTRAINT result_number_of_variables_constraint CHECK (number_of_variables >= 0), number_of_constraints INT NOT NULL CONSTRAINT result_number_of_constraints_constraint CHECK (number_of_constraints >= 0), CONSTRAINT result_unique_constraint UNIQUE (run, problem, model, instance) ON CONFLICT IGNORE)')
     cursor.execute('CREATE INDEX IF NOT EXISTS result_index ON result(run, problem, model, instance)')
 
 def importResults(run, file, cursor):
     results = json.load(file)
     solverStatistics = results.get('solver-statistics')
     cursor.execute(
-        'INSERT INTO result VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        'INSERT INTO result VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
         (run,
          results['task']['suite'],
          results['task']['problem'],
@@ -32,7 +32,9 @@ def importResults(run, file, cursor):
          results['result'].get('violation'),
          results['result'].get('quality'),
          solverStatistics['runtime-in-seconds'] if solverStatistics else None,
-         solverStatistics['moves-per-second'] if solverStatistics else None))
+         solverStatistics['moves-per-second'] if solverStatistics else None,
+         results['yuck-model-statistics']['number-of-search-variables'] + results['yuck-model-statistics']['number-of-channel-variables'],
+         results['yuck-model-statistics']['number-of-constraints']))
 
 def main():
     parser = argparse.ArgumentParser(description = 'Puts Yuck integration test results into database')
