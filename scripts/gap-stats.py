3ed876cf3e937916a1e18bc3033abe1f62dfa54b
Michael Marte <informarte@freenet.de>
2021-09-16 08:20:20 +0200

    Also distinguish results by model

--- scripts/gap-stats.py
+++ scripts/gap-stats.py
@@ -25,47 +25,48 @@ import common
 import re
 
 def computeGaps(cursor, args):
-    jobQuery = 'SELECT DISTINCT problem, instance, problem_type FROM result WHERE run IN (%s) ORDER BY problem, instance' % ','.join('?' for run in args.runs)
+    jobQuery = 'SELECT DISTINCT problem, model, instance, problem_type FROM result WHERE run IN (%s) ORDER BY problem, model, instance' % ','.join('?' for run in args.runs)
     problemPattern = re.compile(args.problemFilter)
+    modelPattern = re.compile(args.modelFilter)
     instancePattern = re.compile(args.instanceFilter)
-    jobs = [(problem, instance, problemType)
-            for (problem, instance, problemType) in cursor.execute(jobQuery, args.runs)
-            if problemPattern.match(problem) and instancePattern.match(instance)]
+    jobs = [(problem, model, instance, problemType)
+            for (problem, model, instance, problemType) in cursor.execute(jobQuery, args.runs)
+            if problemPattern.match(problem) and modelPattern.match(model) and instancePattern.match(instance)]
     if not jobs:
         print('No results found', file = sys.stderr)
         return {}
     results = {}
     for run in args.runs:
-        resultQuery = 'SELECT problem, instance, solved, quality FROM result WHERE run = ?';
+        resultQuery = 'SELECT problem, model, instance, solved, quality FROM result WHERE run = ?';
         for result in cursor.execute(resultQuery, (run,)):
-            (problem, instance, solved, quality) = result
-            if problemPattern.match(problem) and instancePattern.match(instance):
+            (problem, model, instance, solved, quality) = result
+            if problemPattern.match(problem) and modelPattern.match(model) and instancePattern.match(instance):
                 if not run in results:
                     results[run] = {}
-                task = (problem, instance)
+                task = (problem, model, instance)
                 results[run][task] = {'solved': solved, 'quality': quality}
         if not run in results:
             print('Warning: No results found for run {}'.format(run, file = sys.stderr))
             results[run] = {}
         elif len(results[run]) != len(jobs):
             print('Warning: Expected {} results for run {}, but found {}'.format(len(jobs), run, len(results[run])), file = sys.stderr)
-    for (problem, instance, problemType) in jobs:
+    for (problem, model, instance, problemType) in jobs:
         if problemType != 'MIN':
             raise ValueError('Unsupported problem type {}'.format(problemType))
-        task = (problem, instance)
+        task = (problem, model, instance)
         qualities = [int(result['quality']) for result in [results[run][task] for run in results if task in results[run]] if result['quality']]
-        optima = [int(result[0]) for result in cursor.execute('SELECT optimum FROM result WHERE problem = ? AND instance = ? AND optimum IS NOT NULL', task)]
+        optima = [int(result[0]) for result in cursor.execute('SELECT optimum FROM result WHERE problem = ? AND model = ? AND instance = ? AND optimum IS NOT NULL', task)]
         qualities += optima
-        highScores = [int(result[0]) for result in cursor.execute('SELECT high_score FROM result WHERE problem = ? AND instance = ? AND high_score IS NOT NULL', task)]
+        highScores = [int(result[0]) for result in cursor.execute('SELECT high_score FROM result WHERE problem = ? AND model = ? AND instance = ? AND high_score IS NOT NULL', task)]
         qualities += highScores
         bks = None if not qualities else min(qualities)
         if not bks:
-            raise ValueError('{}/{}: No best known solution'.format(problem, instance))
+            raise ValueError('{}/{}/{}: No best known solution'.format(problem, model, instance))
         if bks < 0:
-            raise ValueError('{}/{}: Best known solution has negative objective value'.format(problem, instance))
+            raise ValueError('{}/{}/{}: Best known solution has negative objective value'.format(problem, model, instance))
         if args.verbose:
             print('-' * 80)
-            print(problem, instance, problemType, bks)
+            print(problem, model, instance, problemType, bks)
         for run in results:
             if task in results[run]:
                 result = results[run][task]
@@ -73,9 +74,9 @@ def computeGaps(cursor, args):
                 quality = result['quality']
                 if solved:
                     if not quality:
-                        raise ValueError('{}/{}/{}: No objective value'.format(run, problem, instance))
+                        raise ValueError('{}/{}/{}/{}: No objective value'.format(run, problem, model, instance))
                     if quality < 0:
-                        raise ValueError('{}/{}/{}: Negative objective value'.format(run, problem, instance))
+                        raise ValueError('{}/{}/{}/{}: Negative objective value'.format(run, problem, model, instance))
                     gap = quality / bks
                     if args.verbose:
                         print(run, quality, gap)
@@ -93,7 +94,10 @@ def postprocessResult(result):
 
 def plotDiagrams(args, results):
     title = 'Gaps'
-    filters = ([args.problemFilter] if args.problemFilter else []) + ([args.instanceFilter] if args.instanceFilter else [])
+    filters = \
+        ([args.problemFilter] if args.problemFilter else []) + \
+        ([args.modelFilter] if args.modelFilter else []) + \
+        ([args.instanceFilter] if args.instanceFilter else [])
     if filters:
         title += ' ({})'.format(', '.join(filters))
     common.plotDiagrams(
@@ -111,6 +115,7 @@ def main():
     parser.add_argument('-p', '--plot', dest = 'plotDiagrams', action = 'store_true', help = 'Plot diagrams')
     parser.add_argument('-v', '--verbose', action = 'store_true')
     parser.add_argument('--problem-filter', dest = 'problemFilter', default = '', help = 'Consider only problems that match the given regexp')
+    parser.add_argument('--model-filter', dest = 'modelFilter', default = '', help = 'Consider only models that match the given regexp')
     parser.add_argument('--instance-filter', dest = 'instanceFilter', default = '', help = 'Consider only instances that match the given regexp')
     parser.add_argument('runs', metavar = 'run', nargs = '+')
     args = parser.parse_args()
