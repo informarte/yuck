f35ddd7c5279603d8b5e916a389e20ca60305369
Michael Marte <informarte@freenet.de>
2021-11-15 21:05:49 +0100

    Improve lex_less and lex_lesseq tests

--- src/test/yuck/constraints/test/LexLessTest.scala
+++ src/test/yuck/constraints/test/LexLessTest.scala
@@ -2,7 +2,8 @@ package yuck.constraints.test
 
 import org.junit._
 
-import yuck.constraints.LtRelation
+import yuck.constraints.LexLess
+import yuck.constraints.test.util.ConstraintTestTooling
 import yuck.core._
 import yuck.test.util.UnitTest
 
@@ -11,52 +12,68 @@ import yuck.test.util.UnitTest
  *
  */
 @FixMethodOrder(runners.MethodSorters.NAME_ASCENDING)
-final class LexLessTest extends UnitTest with LexTestTooling[IntegerValue] {
+final class LexLessTest extends UnitTest with ConstraintTestTooling {
 
-    override protected val valueTraits = IntegerValueTraits
-    override protected val space = new  Space(logger, sigint)
+    private val space = new Space(logger, sigint)
+
+    private val baseDomain = CompleteIntegerRange
+    private val xs = for (i <- 1 to 3) yield new IntegerVariable(space.nextVariableId, "x%d".format(i), baseDomain)
+    private val Seq(x1, x2, x3) = xs
+    private val ys = for (i <- 1 to 3) yield new IntegerVariable(space.nextVariableId, "y%d".format(i), baseDomain)
+    private val Seq(y1, y2, y3) = ys
+    private val costs = new BooleanVariable(space.nextVariableId, "costs", CompleteBooleanDomain)
 
     @Test
-    def test33: Unit = {
-        runScenario(
-            TestScenario(
-                LtRelation,
-                3, 3,
-                TestStep(False, (X(1), One), (X(2), One), (X(3), One), (Y(1), One), (Y(2), One), (Y(3), One)),
-                TestStep(True, (X(1), Zero)),
-                TestStep(True, (X(1), One), (X(2), Zero)),
-                TestStep(True, (X(2), One), (X(3), Zero)),
-                TestStep(False, (Y(3), Zero)),
-                TestStep(False2, (Y(2), Zero)),
-                TestStep(False3, (Y(1), Zero))))
+    def testBasics: Unit = {
+        val constraint = new LexLess(space.nextConstraintId, null, xs, ys, costs)
+        assertEq(constraint.toString, "lex_less([x1, x2, x3], [y1, y2, y3])")
+        assertEq(constraint.inVariables.size, 6)
+        assertEq(constraint.inVariables.toSet, xs.toSet.union(ys.toSet))
+        assertEq(constraint.outVariables.size, 1)
+        assertEq(constraint.outVariables.head, costs)
     }
 
     @Test
-    def test23: Unit = {
+    def testCostComputation23: Unit = {
+        space.post(new LexLess(space.nextConstraintId, null, Vector(x1, x2), ys, costs))
         runScenario(
             TestScenario(
-                LtRelation,
-                2, 3,
-                TestStep(True, (X(1), One), (X(2), One), (Y(1), One), (Y(2), One), (Y(3), One)),
-                TestStep(True, (Y(3), Zero)),
-                TestStep(False, (Y(2), Zero)),
-                TestStep(False2, (Y(1), Zero)),
-                TestStep(False, (X(1), Zero)),
-                TestStep(True, (X(2), Zero))))
+                space,
+                Initialize("setup", x1 << 1, x2 << 1, y1 << 1, y2 << 1, y3 << 1, costs << True),
+                ConsultAndCommit("1", y3 << 0, costs << True),
+                ConsultAndCommit("2", y2 << 0, costs << False),
+                ConsultAndCommit("3", y1 << 0, costs << False2),
+                ConsultAndCommit("4", x1 << 0, costs << False),
+                ConsultAndCommit("5", x2 << 0, costs << True)))
     }
 
     @Test
-    def test32: Unit = {
+    def testCostComputation32: Unit = {
+        space.post(new LexLess(space.nextConstraintId, null, xs, Vector(y1, y2), costs))
         runScenario(
             TestScenario(
-                LtRelation,
-                3, 2,
-                TestStep(False, (X(1), One), (X(2), One), (X(3), One), (Y(1), One), (Y(2), One)),
-                TestStep(False, (X(3), Zero)),
-                TestStep(True, (X(2), Zero)),
-                TestStep(True, (X(1), Zero)),
-                TestStep(True, (Y(1), Zero)),
-                TestStep(False, (Y(2), Zero))))
+                space,
+                Initialize("setup", x1 << 1, x2 << 1, x3 << 1, y1 << 1, y2 << 1, costs << False),
+                ConsultAndCommit("1", x3 << 0, costs << False),
+                ConsultAndCommit("2", x2 << 0, costs << True),
+                ConsultAndCommit("3", x1 << 0, costs << True),
+                ConsultAndCommit("4", y1 << 0, costs << True),
+                ConsultAndCommit("5", y2 << 0, costs << False)))
+    }
+
+    @Test
+    def testCostComputation33: Unit = {
+        space.post(new LexLess(space.nextConstraintId, null, xs, ys, costs))
+        runScenario(
+            TestScenario(
+                space,
+                Initialize("setup", x1 << 1, x2 << 1, x3 << 1, y1 << 1, y2 << 1, y3 << 1, costs << False),
+                ConsultAndCommit("1", x1 << 0, costs << True),
+                ConsultAndCommit("2", x1 << 1, x2 << 0, costs << True),
+                ConsultAndCommit("3", x2 << 1, x3 << 0, costs << True),
+                ConsultAndCommit("4", y3 << 0, costs << False),
+                ConsultAndCommit("5", y2 << 0, costs << False2),
+                ConsultAndCommit("6", y1 << 0, costs << False3)))
     }
 
 }
