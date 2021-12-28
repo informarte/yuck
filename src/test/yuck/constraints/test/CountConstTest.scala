8d54a3c8c8828dd999a585550d8b750dba33d429
Michael Marte <informarte@freenet.de>
2021-12-28 12:26:19 +0100

    Improve counting tests

--- src/test/yuck/constraints/test/CountConstTest.scala
+++ src/test/yuck/constraints/test/CountConstTest.scala
@@ -2,8 +2,6 @@ package yuck.constraints.test
 
 import org.junit._
 
-import scala.collection._
-
 import yuck.constraints._
 import yuck.constraints.test.util.ConstraintTestTooling
 import yuck.core._
@@ -16,23 +14,79 @@ import yuck.test.util.UnitTest
 @FixMethodOrder(runners.MethodSorters.NAME_ASCENDING)
 final class CountConstTest extends UnitTest with ConstraintTestTooling {
 
+    private val space = new Space(logger, sigint)
+
+    private val baseDomain = IntegerRange(0, 9)
+    private val xs = for (i <- 1 to 3) yield new IntegerVariable(space.nextVariableId, "x%d".format(i), baseDomain)
+    private val Seq(x1, x2, x3) = xs
+    private val n = new IntegerVariable(space.nextVariableId, "n", CompleteIntegerRange)
+
     @Test
-    def testCountConst: Unit = {
-        val space = new Space(logger, sigint)
-        val d = IntegerRange(Zero, Nine)
-        val s = space.createVariable("s", d)
-        val t = space.createVariable("t", d)
-        val u = space.createVariable("u", d)
-        val n = new IntegerVariable(space.nextVariableId, "n", NonNegativeIntegerRange)
-        space.post(new CountConst(space.nextConstraintId, null, List(s, t, u), One, n))
-        assertEq(space.searchVariables, Set(s, t, u))
+    def testBasics: Unit = {
+        val constraint = new CountConst(space.nextConstraintId, null, xs, One, n)
+        assertEq(constraint.toString, "n = count(1, [x1, x2, x3])")
+        assertEq(constraint.inVariables.size, xs.size)
+        assertEq(constraint.inVariables.toSet, xs.toSet)
+        assertEq(constraint.outVariables.size, 1)
+        assertEq(constraint.outVariables.head, n)
+    }
+
+    @Test
+    def testPropagation: Unit = {
+        space.post(new CountConst(space.nextConstraintId, null, xs, One, n))
         runScenario(
             TestScenario(
                 space,
-                Initialize("setup", (s, One), (t, One), (u, One), (n, Three)),
-                ConsultAndCommit("1", (s, Two), (n, Two)),
-                ConsultAndCommit("2", (s, One), (u, Two), (n, Two)),
-                ConsultAndCommit("3", (u, One), (n, Three))))
+                Propagate("root-node propagation", Nil, List(n << (0, 3))),
+                PropagateAndRollback("1", List(x1 << (0, 0)), List(n << (0, 2))),
+                PropagateAndRollback("2", List(x2 << (1, 1)), List(n << (1, 3))),
+                PropagateAndRollback("3", List(x1 << (0, 0), x2 << (1, 1)), List(n << (1, 2))),
+                PropagateAndRollback("4", List(n << (3, 3)), List(x1 << (1, 1), x2 << (1, 1), x3 << (1, 1))),
+                Propagate("5", List(x3 << (0, 0), n << (2, 2)), List(x1 << (1, 1), x2 << (1, 1)))))
+    }
+
+    @Test
+    def testHandlingOfDuplicateVariablesInPropagation: Unit = {
+        space.post(new CountConst(space.nextConstraintId, null, List(x1, x1, x2, x3), One, n))
+        runScenario(
+            TestScenario(
+                space,
+                Propagate("root-node propagation", Nil, List(n << (0, 4))),
+                PropagateAndRollback("1", List(x1 << (0, 0)), List(n << (0, 2))),
+                PropagateAndRollback("2", List(x2 << (1, 1)), List(n << (1, 4))),
+                PropagateAndRollback("3", List(x1 << (0, 0), x2 << (1, 1)), List(n << (1, 2))),
+                PropagateAndRollback("4", List(n << (4, 4)), List(x1 << (1, 1), x2 << (1, 1), x3 << (1, 1))),
+                Propagate("5", List(x3 << (0, 0), n << (3, 3)), List(x1 << (1, 1), x2 << (1, 1)))))
+    }
+
+    @Test
+    def testCounting: Unit = {
+        space.post(new CountConst(space.nextConstraintId, null, xs, One, n))
+        runScenario(
+            TestScenario(
+                space,
+                Initialize("1", x1 << 2, x2 << 2, x3 << 2, n << 0),
+                Initialize("2", x1 << 1, x2 << 1, x3 << 1, n << 3),
+                Consult("1", x1 << 2, n << 2),
+                Consult("2", x1 << 2, x3 << 2, n << 1),
+                ConsultAndCommit("1", x1 << 2, n << 2),
+                ConsultAndCommit("2", x1 << 1, x3 << 2, n << 2),
+                ConsultAndCommit("3", x3 << 1, n << 3)))
+    }
+
+    @Test
+    def testHandlingOfDuplicateVariablesInCounting: Unit = {
+        space.post(new CountConst(space.nextConstraintId, null, List(x1, x1, x2, x3), One, n))
+        runScenario(
+            TestScenario(
+                space,
+                Initialize("1", x1 << 2, x2 << 2, x3 << 2, n << 0),
+                Initialize("2", x1 << 1, x2 << 1, x3 << 1, n << 4),
+                Consult("1", x1 << 2, n << 2),
+                Consult("2", x1 << 2, x3 << 2, n << 1),
+                ConsultAndCommit("1", x1 << 2, n << 2),
+                ConsultAndCommit("2", x1 << 1, x3 << 2, n << 3),
+                ConsultAndCommit("3", x3 << 1, n << 4)))
     }
 
 }
--- src/test/yuck/constraints/test/CountVarTest.scala
+++ src/test/yuck/constraints/test/CountVarTest.scala
@@ -2,8 +2,6 @@ package yuck.constraints.test
 
 import org.junit._
 
-import scala.collection._
-
 import yuck.constraints._
 import yuck.constraints.test.util.ConstraintTestTooling
 import yuck.core._
@@ -16,25 +14,54 @@ import yuck.test.util.UnitTest
 @FixMethodOrder(runners.MethodSorters.NAME_ASCENDING)
 final class CountVarTest extends UnitTest with ConstraintTestTooling {
 
+    private val space = new Space(logger, sigint)
+
+    private val baseDomain = IntegerRange(0, 9)
+    private val xs = for (i <- 1 to 3) yield new IntegerVariable(space.nextVariableId, "x%d".format(i), baseDomain)
+    private val Seq(x1, x2, x3) = xs
+    private val y = new IntegerVariable(space.nextVariableId, "y", baseDomain)
+    private val n = new IntegerVariable(space.nextVariableId, "n", NonNegativeIntegerRange)
+
     @Test
-    def testCountVar: Unit = {
-        val space = new Space(logger, sigint)
-        val d = IntegerRange(Zero, Nine)
-        val s = space.createVariable("s", d)
-        val t = space.createVariable("t", d)
-        val u = space.createVariable("u", d)
-        val what = space.createVariable("what", d)
-        val n = new IntegerVariable(space.nextVariableId, "n", NonNegativeIntegerRange)
-        space.post(new CountVar(space.nextConstraintId, null, List(s, t, u), what, n))
-        assertEq(space.searchVariables, Set(s, t, u, what))
+    def testBasics: Unit = {
+        val constraint = new CountVar(space.nextConstraintId, null, xs, y, n)
+        assertEq(constraint.toString, "n = count(y, [x1, x2, x3])")
+        assertEq(constraint.inVariables.size, xs.size + 1)
+        assertEq(constraint.inVariables.toSet, Set(y).concat(xs))
+        assertEq(constraint.outVariables.size, 1)
+        assertEq(constraint.outVariables.head, n)
+    }
+
+    @Test
+    def testCounting: Unit = {
+        space.post(new CountVar(space.nextConstraintId, null, xs, y, n))
         runScenario(
             TestScenario(
                 space,
-                Initialize("setup", (s, One), (t, One), (u, One), (what, One), (n, Three)),
-                ConsultAndCommit("1", (s, Two), (n, Two)),
-                ConsultAndCommit("2", (s, One), (u, Two), (n, Two)),
-                ConsultAndCommit("3", (what, Two), (n, One)),
-                ConsultAndCommit("4", (what, One), (u, One), (n, Three))))
+                Initialize("1", x1 << 2, x2 << 3, x3 << 3, y << 1, n << 0),
+                Initialize("2", x1 << 1, x2 << 1, x3 << 1, y << 1, n << 3),
+                Consult("1", x1 << 2, n << 2),
+                Consult("2", x1 << 2, y << 2, n << 1),
+                ConsultAndCommit("1", x1 << 2, n << 2),
+                ConsultAndCommit("2", x1 << 1, x3 << 2, n << 2),
+                ConsultAndCommit("3", y << 2, n << 1),
+                ConsultAndCommit("4", y << 1, x3 << 1, n << 3)))
+    }
+
+    @Test
+    def testHandlingOfDuplicateVariablesInCounting: Unit = {
+        space.post(new CountVar(space.nextConstraintId, null, List(x1, x1, x2, x3), y, n))
+        runScenario(
+            TestScenario(
+                space,
+                Initialize("1", x1 << 2, x2 << 3, x3 << 3, y << 1, n << 0),
+                Initialize("2", x1 << 1, x2 << 1, x3 << 1, y << 1, n << 4),
+                Consult("1", x1 << 2, n << 2),
+                Consult("2", x1 << 2, y << 2, n << 2),
+                ConsultAndCommit("1", x1 << 2, n << 2),
+                ConsultAndCommit("2", x1 << 1, x3 << 2, n << 3),
+                ConsultAndCommit("3", y << 2, n << 1),
+                ConsultAndCommit("4", y << 1, x3 << 1, n << 4)))
     }
 
 }
