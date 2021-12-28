9308fe366c1728b0d72e5dc4bee4db9898ccde03
Michael Marte <informarte@freenet.de>
2021-12-28 12:28:44 +0100

    Improve nvalue tests

--- src/test/yuck/constraints/test/NumberOfDistinctValuesTest.scala
+++ src/test/yuck/constraints/test/NumberOfDistinctValuesTest.scala
@@ -14,21 +14,34 @@ import yuck.test.util.UnitTest
 @FixMethodOrder(runners.MethodSorters.NAME_ASCENDING)
 final class NumberOfDistinctValuesTest extends UnitTest with ConstraintTestTooling {
 
-    @Test
-    def testNumberOfDistinctValues: Unit = {
-        val space = new Space(logger, sigint)
-        val d = IntegerRange(One, Two)
-        val xs = for (i <- 1 to 3) yield space.createVariable("x[%d]".format(i), d)
-        val m = new IntegerVariable(space.nextVariableId, "m", NonNegativeIntegerRange)
-        space.post(new NumberOfDistinctValues(space.nextConstraintId, null, xs, m))
-        assertEq(space.searchVariables, xs.toSet)
+    private val space = new Space(logger, sigint)
+
+    private val baseDomain = IntegerRange(1, 2)
+    private val xs = for (i <- 1 to 3) yield new IntegerVariable(space.nextVariableId, "x%d".format(i), baseDomain)
+    private val Seq(x1, x2, x3) = xs
+    private val n = new IntegerVariable(space.nextVariableId, "n", NonNegativeIntegerRange)
+
+    private def testCounting(xs: Seq[IntegerVariable]): Unit = {
+        space.post(new NumberOfDistinctValues(space.nextConstraintId, null, xs, n))
         runScenario(
             TestScenario(
                 space,
-                Initialize("setup", (xs(0), One), (xs(1), One),  (xs(2), One), (m, One)),
-                ConsultAndCommit("1", (xs(0), Two), (m, Two)),
-                ConsultAndCommit("2", (xs(1), Two), (m, Two)),
-                ConsultAndCommit("3", (xs(2), Two), (m, One))))
+                Initialize("1", x1 << 1, x2 << 2, x3 << 3, n << 3),
+                Initialize("2", x1 << 1, x2 << 1, x3 << 1, n << 1),
+                Consult("1", x1 << 2, n << 2),
+                Consult("2", x2 << 2, x3 << 2, n << 2),
+                ConsultAndCommit("1", x1 << 2, n << 2),
+                ConsultAndCommit("2", x2 << 2, x3 << 2, n << 1)))
+    }
+
+    @Test
+    def testCounting: Unit = {
+        testCounting(xs)
+    }
+
+    @Test
+    def testHandlingOfDuplicateVariablesInCounting: Unit = {
+        testCounting(List(x1, x2, x2, x3))
     }
 
 }
