7a23356a608270dc18ca96e68c9193c45ab76cfc
Michael Marte <informarte@freenet.de>
2022-02-19 22:00:55 +0100

    Split some large files

---
+++ src/main/yuck/constraints/GeneralInverseNeighbourhood.scala
@@ -0,0 +1,73 @@
+package yuck.constraints
+
+import yuck.core.*
+
+/**
+ * This neighbourhood can be used to maintain an ''inverse'' constraint where
+ * f and g do not share variables.
+ *
+ * @author Michael Marte
+ */
+final class GeneralInverseNeighbourhood
+    (space: Space, f: InverseFunction, g: InverseFunction, randomGenerator: RandomGenerator)
+    extends InverseNeighbourhood(space, f, g)
+{
+
+    require(f.xs.toSet.intersect(g.xs.toSet).isEmpty)
+
+    private val effects = Vector.fill(4){new ReusableMoveEffect[IntegerValue]}
+    private val candidates1 =
+         f.xs.indices
+        .filter(i => f.xs(i).domain.size > 1 && g.xs(rawValue(f.xs(i)) - g.offset).domain.size > 1)
+
+    override def nextMove = {
+        if (candidates1.isEmpty) {
+            new ChangeValues[IntegerValue](space.nextMoveId, Nil)
+        } else {
+            val i1 = candidates1(randomGenerator.nextInt(candidates1.size))
+            val x1 = f.xs(i1)
+            val a1 = value(x1)
+            val j1 = a1.value - g.offset
+            val y1 = g.xs(j1)
+            val b1 = value(y1)
+            assert(b1.value - f.offset == i1)
+            val candidates2 =
+                y1
+                .domain
+                .valuesIterator
+                .map(_.value - f.offset)
+                .filter(i2 => i2 != i1)
+                .filter(
+                    i2 => {
+                        val x2 = f.xs(i2)
+                        val a2 = value(x2)
+                        val j2 = a2.value - g.offset
+                        val y2 = g.xs(j2)
+                        val b2 = value(y2)
+                        assert(b2.value - f.offset == i2)
+                        x1.domain.contains(a2) && x2.domain.contains(a1) &&
+                        y1.domain.contains(b2) && y2.domain.contains(b1)
+                    })
+                .toIndexedSeq
+            if (candidates2.isEmpty) {
+                new ChangeValues[IntegerValue](space.nextMoveId, Nil)
+            } else {
+                val i2 = candidates2(randomGenerator.nextInt(candidates2.size))
+                val x2 = f.xs(i2)
+                val a2 = value(x2)
+                val j2 = a2.value - g.offset
+                val y2 = g.xs(j2)
+                val b2 = value(y2)
+                assert(b2.value - f.offset == i2)
+                // {(x1, y1), (x2, y2)} -> {(x1, y2), (x2, y1)}
+                effects(0).set(x1, a2)
+                effects(1).set(x2, a1)
+                effects(2).set(y1, b2)
+                effects(3).set(y2, b1)
+                new ChangeValues(space.nextMoveId, effects)
+            }
+        }
+    }
+
+}
+
---
+++ src/main/yuck/constraints/InverseNeighbourhood.scala
@@ -0,0 +1,33 @@
+package yuck.constraints
+
+import yuck.core._
+
+/**
+ * @author Michael Marte
+ *
+ */
+abstract class InverseNeighbourhood
+    (protected val space: Space,
+     protected val f: InverseFunction,
+     protected val g: InverseFunction)
+    extends Neighbourhood
+{
+
+    protected final def value(x: IntegerVariable): IntegerValue = space.searchState.value(x)
+    protected final def rawValue(x: IntegerVariable): Int = value(x).value
+
+    require(f.xs.size == g.xs.size)
+    require(f.isSuitableForImplicitSolving(space))
+    require(g.isSuitableForImplicitSolving(space))
+    require(f.xs.forall(x => x.domain.isSubsetOf(g.indexDomain)))
+    require(g.xs.forall(x => x.domain.isSubsetOf(f.indexDomain)))
+    require(f.xs.forall(x => x.domain.contains(value(x))))
+    require(g.xs.forall(x => x.domain.contains(value(x))))
+    require(f.indexRange.forall(i => rawValue(g.xs(rawValue(f.xs(i - f.offset)) - g.offset)) == i))
+
+    final override def searchVariables = (f.xs.iterator ++ g.xs.iterator).filterNot(_.domain.isSingleton).toSet
+
+    final override def children = Nil
+
+}
+
