aee43507e9053e0160ebddae92ec3df11282a0c8
Michael Marte <informarte@freenet.de>
2022-02-20 18:07:53 +0100

    Abandon !!!

--- src/main/yuck/core/EmptyIntegerSetDomain.scala
+++ src/main/yuck/core/EmptyIntegerSetDomain.scala
@@ -11,10 +11,19 @@ object EmptyIntegerSetDomain extends IntegerSetDomain {
     override val ub = EmptyIntegerSetValue
     override def hull = this
     override def values = None
-    override def singleValue = !!!
+    override def singleValue = {
+        require(false)
+        ???
+    }
     override def contains(a: IntegerSetValue) = false
-    override def randomValue(randomGenerator: RandomGenerator) = !!!
-    override def nextRandomValue(randomGenerator: RandomGenerator, currentValue: IntegerSetValue) = !!!
+    override def randomValue(randomGenerator: RandomGenerator) = {
+        require(false)
+        ???
+    }
+    override def nextRandomValue(randomGenerator: RandomGenerator, currentValue: IntegerSetValue) = {
+        require(false)
+        ???
+    }
     override def size = 0
     override def isComplete = false
     override def isFinite = true
