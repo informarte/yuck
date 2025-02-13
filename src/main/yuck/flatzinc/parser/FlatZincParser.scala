62b7f1f8649ccd63f6d43772332585d5f0721411
Michael Marte <informarte@freenet.de>
2025-02-13 08:11:54 +0100

    No more need for lazy vals in FlatZinc parser

--- src/main/yuck/flatzinc/parser/FlatZincParser.scala
+++ src/main/yuck/flatzinc/parser/FlatZincParser.scala
@@ -13,11 +13,11 @@ import yuck.flatzinc.ast.*
  */
 object FlatZincParser extends RegexParsers {
 
-    lazy val bool_const: Parser[BoolConst] =
+    val bool_const: Parser[BoolConst] =
         "true" ^^^ BoolConst(true) | "false" ^^^ BoolConst(false)
-    lazy val identifier: Parser[String] =
+    val identifier: Parser[String] =
         regex(new Regex("_*[A-Za-z][A-Za-z0-9_]*"))
-    lazy val int_const: Parser[IntConst] =
+    val int_const: Parser[IntConst] =
         regex(new Regex("(\\+|-)?[0-9]+")) ^? (
             new PartialFunction[String, IntConst]() {
                 override def isDefinedAt(s: String) =
@@ -26,11 +26,11 @@ object FlatZincParser extends RegexParsers {
                     IntConst(s.toLong)
             },
             s => String.format("Invalid integer literal %s", s))
-    lazy val float_const_with_fractional_part: Parser[String] =
+    val float_const_with_fractional_part: Parser[String] =
         regex(new Regex("(\\+|-)?[0-9]+\\.[0-9]+((e|E)(\\+|-)?[0-9]+)?"))
-    lazy val float_const_without_fractional_part: Parser[String] =
+    val float_const_without_fractional_part: Parser[String] =
         regex(new Regex("(\\+|-)?[0-9]+(e|E)(\\+|-)?[0-9]+"))
-    lazy val float_const: Parser[FloatConst] =
+    val float_const: Parser[FloatConst] =
         (float_const_without_fractional_part | float_const_with_fractional_part) ^? (
             new PartialFunction[String, FloatConst]() {
                 override def isDefinedAt(s: String) =
@@ -40,45 +40,45 @@ object FlatZincParser extends RegexParsers {
                     FloatConst(s.toDouble)
             },
             s => String.format("Invalid float literal %s", s))
-    lazy val int_range: Parser[IntRange] =
+    val int_range: Parser[IntRange] =
         int_const ~ ".." ~ int_const ^^ {
             case IntConst(lb) ~ _ ~ IntConst(ub) => IntRange(lb, ub)
         }
-    lazy val index_set: Parser[Option[IntRange]] =
+    val index_set: Parser[Option[IntRange]] =
         "[" ~> int_range <~ "]" ^^ (range => Some(range)) |
         "[" ~ repsep("int", ",") ~ "]" ^^^ None
-    lazy val int_set: Parser[IntSet] =
+    val int_set: Parser[IntSet] =
         "{" ~> repsep(int_const, ",") <~ "}" ^^ {
             l => IntSet((for (IntConst(e) <- l) yield e).toSet)
         }
-    lazy val float_range: Parser[FloatRange] =
+    val float_range: Parser[FloatRange] =
         float_const ~ ".." ~ float_const ^^ {
             case FloatConst(lb) ~ _ ~ FloatConst(ub) => FloatRange(lb, ub)
         }
-    lazy val int_set_const: Parser[IntSetConst] =
+    val int_set_const: Parser[IntSetConst] =
         int_range ^^ (r => IntSetConst(r)) |
         int_set ^^ (s => IntSetConst(s))
-    lazy val array_const: Parser[ArrayConst] =
+    val array_const: Parser[ArrayConst] =
         "[" ~> repsep(expr, ",") <~ "]" ^^ (elems => ArrayConst(elems.toVector))
-    lazy val array_access: Parser[ArrayAccess] =
+    val array_access: Parser[ArrayAccess] =
         identifier ~ ("[" ~> expr <~ "]") ^^ {
             case id ~ idx => ArrayAccess(id, idx)
         }
     // limited string parsing only
-    lazy val string_const: Parser[StringConst] =
+    val string_const: Parser[StringConst] =
         "\"" ~> identifier <~ "\"" ^^ StringConst.apply
-    lazy val term: Parser[Term] =
+    val term: Parser[Term] =
         identifier ~ (("(" ~> rep1sep(expr, ",") <~ ")")?) ^^ {
             case id ~ optionalParams => Term(id, optionalParams.getOrElse(Nil))
         }
-    lazy val expr: Parser[Expr] =
+    val expr: Parser[Expr] =
         (bool_const | float_const | int_set_const | int_const | array_const | array_access | string_const | term)
 
     // type parsing
     // According to the FlatZinc grammar, not every type applies in every context.
     // I ignore these restrictions here because I think that the type checker is the
     // better place to implement them.
-    lazy val param_base_type: Parser[BaseType] =
+    val param_base_type: Parser[BaseType] =
         "bool" ^^^ BoolType |||
         "int" ^^^ IntType(None) |||
         "float" ^^^ FloatType(None) |||
@@ -89,51 +89,51 @@ object FlatZincParser extends RegexParsers {
             (int_range ^^ (r => IntSetType(Some(r))) |||
              int_set ^^ (s => IntSetType(Some(s))) |||
              "int" ^^^ IntSetType(None))
-    lazy val param_array_type: Parser[ArrayType] =
+    val param_array_type: Parser[ArrayType] =
         array_type(param_base_type)
-    lazy val param_type: Parser[Type] =
+    val param_type: Parser[Type] =
         param_base_type | param_array_type
-    lazy val var_base_type: Parser[BaseType] =
+    val var_base_type: Parser[BaseType] =
         "var" ~> param_base_type
-    lazy val var_array_type: Parser[ArrayType] =
+    val var_array_type: Parser[ArrayType] =
         array_type(var_base_type)
-    lazy val var_type: Parser[Type] =
+    val var_type: Parser[Type] =
         var_base_type | var_array_type
     def array_type(baseTypeParser: Parser[BaseType]): Parser[ArrayType] =
         ("array" ~> index_set) ~ ("of" ~> baseTypeParser) ^^ {
             case indexSet ~ baseType => ArrayType(indexSet, baseType)
         }
 
-    lazy val pred_param_type: Parser[Type] =
+    val pred_param_type: Parser[Type] =
         param_type | var_type
-    lazy val pred_param: Parser[PredParam] =
+    val pred_param: Parser[PredParam] =
         (pred_param_type <~ ":") ~ identifier ~ (annotation*) ^^ {
             case paramType ~ id ~ annotations => PredParam(id, paramType, annotations)
         }
-    lazy val pred_decl: Parser[PredDecl] =
+    val pred_decl: Parser[PredDecl] =
         "predicate" ~> identifier ~ ("(" ~> repsep(pred_param, ",") <~ ")") <~ ";" ^^ {
             case id ~ params=> PredDecl(id, params)
         }
 
-    lazy val param_decl: Parser[ParamDecl] =
+    val param_decl: Parser[ParamDecl] =
         (param_type <~ ":") ~ identifier ~ ("=" ~> expr) <~ ";" ^^ {
             case paramType ~ id ~ value => ParamDecl(id, paramType, value)
         }
 
-    lazy val var_decl: Parser[VarDecl] =
+    val var_decl: Parser[VarDecl] =
         (var_type <~ ":") ~ identifier ~ (annotation*) ~ (("=" ~> expr)?) <~ ";" ^^ {
             case paramType ~ id ~ annotations ~ optionalValue => VarDecl(id, paramType, optionalValue, annotations)
         }
 
-    lazy val annotation: Parser[Annotation] =
+    val annotation: Parser[Annotation] =
         "::" ~> term ^^ Annotation.apply
 
-    lazy val constraint: Parser[Constraint] =
+    val constraint: Parser[Constraint] =
         "constraint" ~> identifier ~ ("(" ~> rep1sep(expr, ",") <~ ")") ~ (annotation*) <~ ";" ^^ {
             case id ~ params ~ annotations => Constraint(id, params, annotations)
         }
 
-    lazy val solve_goal: Parser[SolveGoal] =
+    val solve_goal: Parser[SolveGoal] =
         "solve" ~> (
             (annotation*) <~ "satisfy" ^^ Satisfy.apply |
             (annotation*) ~ ("minimize" ~> expr) ^^ {
@@ -143,7 +143,7 @@ object FlatZincParser extends RegexParsers {
                 case annotations ~ expr => Maximize(expr, annotations)
             }) <~ ";"
 
-    lazy val flatzinc_model: Parser[FlatZincAst] =
+    val flatzinc_model: Parser[FlatZincAst] =
         (pred_decl*)  ~ (param_decl*) ~ (var_decl*) ~ (constraint*)  ~ solve_goal ^^ {
             case predDecls ~ paramDecls ~ varDecls ~ constraints ~ solveGoal =>
                 FlatZincAst(
