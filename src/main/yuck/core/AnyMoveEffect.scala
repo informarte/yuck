35d0888d72fc73a9cd4a0c99f6f5e32fca6f883d
Michael Marte <informarte@freenet.de>
2025-12-13 18:34:59 +0100

    Explain how to inspect effects

--- src/main/yuck/core/AnyMoveEffect.scala
+++ src/main/yuck/core/AnyMoveEffect.scala
@@ -4,6 +4,12 @@ import scala.collection.mutable.Cloneable
 
 /**
  * Describes the effect of a move on the value of a variable.
+ *
+ * Notice that AnyMoveEffect is an iterable of itself.
+ * (This helps to avoid object allocations.)
+ * Therefore, by default, the debugger of IntelliJ IDEA will not show x and a
+ * but the elements of the iterable, i.e. the effect itself.
+ * To inspect x and a, select the effect and then View as > Scala object.
  */
 abstract class AnyMoveEffect extends Cloneable[AnyMoveEffect] with Iterable[AnyMoveEffect] {
 
